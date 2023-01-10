4caa04ffeec9ce56cd80cf7a0766d802357ebfbb
Omg577 <omggd13@gmail.com>
2023-01-09 20:43:47 -0800

    Create SwerveDrive.java
    Co-Authored-By: GuestUser1000101 <122332978+GuestUser1000101@users.noreply.github.com>
    Co-Authored-By: Reshma Winter <119987807+reshmawinter@users.noreply.github.com>
    Co-Authored-By: florayasmin <55338271+florayasmin@users.noreply.github.com>
    Co-Authored-By: Ethan Schroeder <74690582+TheGreaterGoo@users.noreply.github.com>

---
+++ src/main/java/frc/robot/drive/SwerveDrive.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.drive;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase;
+
+/** Add your docs here. */
+public class SwerveDrive extends RobotDriveBase {
+
+    private SwerveModule[] modules;
+
+    /**
+     * constructs the swerve drive
+     * 
+     * @param modules An array of four SwerveModule objects in the order: front left, front right, back left, back right
+     */
+    public SwerveDrive(SwerveModule[] modules) {
+        this.modules = modules;
+    }
+
+    /**
+     * Sets the swerve module states.
+     * 
+     * @param states array of 4 SwerveModuleStates objects
+     */
+    public void setModuleStates(SwerveModuleState[] states) {
+        for(int i=0; i<modules.length;++i){
+            modules[i].setModuleState(states[i]);
+        }   
+    }
+    
+    @Override
+    public void stopMotor() {
+        for (SwerveModule module : modules) {
+            module.stopMotors();
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "SwerveDrive";
+    }
+
+}
