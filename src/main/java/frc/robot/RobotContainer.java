0c34e9473440809a1be25879242b01b85e819a69
Omg577 <omggd13@gmail.com>
2023-01-14 10:30:49 -0800

    Changed new command XboxController

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,46 +18,32 @@ import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
 
-  private final XboxController driveController = new XboxController(0);
-  private JoystickButton driverButtonA = new JoystickButton(driveController, 1); // A Button
-  private JoystickButton driverButtonB = new JoystickButton(driveController, 2); // B Button
-  private JoystickButton driverButtonX = new JoystickButton(driveController, 3); // X Button
-  private JoystickButton driverButtonY = new JoystickButton(driveController, 4); // Y Button
-  private JoystickButton driverLeftBumper = new JoystickButton(driveController, 5);
-  private JoystickButton driverRightBumper = new JoystickButton(driveController, 6);
-  // Left Middle Button
-  private JoystickButton driverStartButton = new JoystickButton(driveController, 7);
-  // Right Middle Button
-  private JoystickButton driverMenuButton = new JoystickButton(driveController, 8);
-
-  private POVButton driverDpadUp = new POVButton(driveController, 0);
-  private POVButton driverDpadRight = new POVButton(driveController, 90);
-  private POVButton driverDpadDown = new POVButton(driveController, 180);
-  private POVButton driverDpadLeft = new POVButton(driveController, 270);
-
-  
+  // new drive controller
+  private final CommandXboxController driveController = new CommandXboxController(0);
 
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
-  
-  
+
   private DriveWithController driveWithController = new DriveWithController(swerveSubsystem, driveController);
 
-  
-
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_driverController = new CommandXboxController(
+      OperatorConstants.kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     swerveSubsystem.setDefaultCommand(driveWithController);
     // Configure the trigger bindings
@@ -65,12 +51,17 @@ public class RobotContainer {
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
@@ -78,7 +69,8 @@ public class RobotContainer {
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
--- src/main/java/frc/robot/commands/DriveWithController.java
+++ src/main/java/frc/robot/commands/DriveWithController.java
@@ -4,22 +4,21 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class DriveWithController extends CommandBase {
 
   private SwerveSubsystem swerveDrive;
-  private XboxController driveController;
+  private CommandXboxController driveController;
 
   /** Creates a new DriveWithController. */
-  public DriveWithController(SwerveSubsystem sDrive, XboxController dController) {
+  public DriveWithController(SwerveSubsystem sDrive, CommandXboxController dController) {
     // Use addRequirements() here to declare subsystem dependencies.
     swerveDrive = sDrive;
     driveController = dController;
     addRequirements(swerveDrive);
-
   }
 
   // Called when the command is initially scheduled.
@@ -28,10 +27,11 @@ public class DriveWithController extends CommandBase {
   }
 
   // Called every time the scheduler runs while the command is scheduled.
-  // TODO? : add tolerance value to execute() so it does not 
+  // TODO? : add tolerance value to execute() so it does not
   @Override
   public void execute() {
-    swerveDrive.drive(-driveController.getLeftY(), -driveController.getLeftX(), -driveController.getRightX(), true, false);
+    swerveDrive.drive(-driveController.getLeftY(), -driveController.getLeftX(), -driveController.getRightX(), true,
+        false);
   }
 
   // Called once the command ends or is interrupted.
