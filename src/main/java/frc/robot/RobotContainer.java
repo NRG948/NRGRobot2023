5cd3a810b2a7b650b7d3d47c106884568dced119
NRG Programmer <newportrobotics@gmail.com>
2023-01-14 09:27:16 -0800

    Add DriveWithController command
    Co-Authored-By: harry695
    Co-Authored-By: AudreyZ19
    Co-Authored-By: GuestUser1000101
    Co-Authored-By: Omg577

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,10 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.DriveWithController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -44,6 +46,12 @@ public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+  
+  
+  private DriveWithController driveWithController = new DriveWithController(swerveSubsystem, driveController);
+
+  
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -51,6 +59,7 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    swerveSubsystem.setDefaultCommand(driveWithController);
     // Configure the trigger bindings
     configureBindings();
   }
---
+++ src/main/java/frc/robot/commands/DriveWithController.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class DriveWithController extends CommandBase {
+
+  private SwerveSubsystem swerveDrive;
+  private XboxController driveController;
+
+  /** Creates a new DriveWithController. */
+  public DriveWithController(SwerveSubsystem sDrive, XboxController dController) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    swerveDrive = sDrive;
+    driveController = dController;
+    addRequirements(swerveDrive);
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  // TODO? : add tolerance value to execute() so it does not 
+  @Override
+  public void execute() {
+    swerveDrive.drive(-driveController.getLeftY(), -driveController.getLeftX(), -driveController.getRightX(), true, false);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    System.out.println("END DriveWithController interrupted:" + interrupted);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
