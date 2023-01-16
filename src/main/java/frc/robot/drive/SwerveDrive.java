757015558e30a512d21e9fcf50bf0cbeb7b955a7
Ed Reed <reed.ej@hotmail.com>
2023-01-15 16:33:57 -0800

    Organize imports

--- src/main/java/frc/robot/drive/SwerveDrive.java
+++ src/main/java/frc/robot/drive/SwerveDrive.java
@@ -10,11 +10,10 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.drive.RobotDriveBase;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
 
 
 /** SwerveDrive implements swerve drive control. */
