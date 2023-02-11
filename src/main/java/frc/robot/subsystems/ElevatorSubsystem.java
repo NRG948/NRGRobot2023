2cb8bd945342d0391731be8674232093a876ca7d
NRG Programmer <newportrobotics@gmail.com>
2023-02-11 11:24:12 -0800

    Integrated Elevator and implemented motion profile control Co-Authored-By: AAQ948 Co-Authored-By: Harry695 Co-Authored-By: saiawe2021

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,11 +4,21 @@
 
 package frc.robot.subsystems;
 
+import java.util.function.Supplier;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RobotConstants;
 import frc.robot.Constants.RobotConstants.CAN;
 import frc.robot.parameters.MotorParameters;
 
@@ -17,12 +27,35 @@ import frc.robot.parameters.MotorParameters;
  * acquiring or scoring game elements.
  */
 public class ElevatorSubsystem extends SubsystemBase {
-
+  /**
+   *
+   */
+  private static final MotorParameters MOTOR = MotorParameters.NeoV1_1;
+  private static final double GEAR_RATIO = 4; // TODO: Get real ratio
+  private static final double PULLEY_DIAMETER = Units.inchesToMeters(2); // TODO: Get real value
+  private static final double ELEVATOR_MASS = 1; // TODO: Get real mass
+  private static final double MAX_SPEED = (MOTOR.getFreeSpeedRPM()*PULLEY_DIAMETER*Math.PI)/(60*GEAR_RATIO);
+  private static final double MAX_ACCELERATION = (2*MOTOR.getStallTorque()*GEAR_RATIO) / (PULLEY_DIAMETER*ELEVATOR_MASS);
+  private static final double KS = 0.15;
+  private static final double KV = (RobotConstants.MAX_BATTERY_VOLTAGE-KS)/MAX_SPEED;
+  private static final double KA = (RobotConstants.MAX_BATTERY_VOLTAGE-KS)/MAX_ACCELERATION;
+  private static final double KG = 9.81*KA;
+  private static final TrapezoidProfile.Constraints CONSTRAINTS = new TrapezoidProfile.Constraints(MAX_SPEED, MAX_ACCELERATION);
+  
+  
   private final CANSparkMax motor;
   private final RelativeEncoder encoder;
+  private final Supplier<Rotation2d> angle;
+  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(KS, KV, KA);
+  private final ProfiledPIDController pidController = new ProfiledPIDController(1.0, 0, 0, CONSTRAINTS);
+  private final Timer timer = new Timer();
 
-  private GoalState goalState;
+  private GoalState goalState = GoalState.ACQUIRE;
+  private TrapezoidProfile profile = new TrapezoidProfile(CONSTRAINTS, new TrapezoidProfile.State(goalState.getPosition(), 0));
   private double currentPosition;
+  private double currentVelocity;
+  private Rotation2d currentAngle;
+
 
   public enum GoalState {
     ACQUIRE(0),
@@ -47,9 +80,10 @@ public class ElevatorSubsystem extends SubsystemBase {
   }
 
   /** Creates a new ElevatorSubsystem. */
-  public ElevatorSubsystem() {
+  public ElevatorSubsystem(Supplier <Rotation2d> angleSupplier) {
     motor = new CANSparkMax(CAN.SparkMax.ELEVATOR, MotorType.kBrushless);
-    encoder = motor.getAlternateEncoder(MotorParameters.NeoV1_1.getPulsesPerRevolution());
+    encoder = motor.getAlternateEncoder(MOTOR.getPulsesPerRevolution());
+    angle = angleSupplier;
   }
 
   /**
@@ -59,10 +93,24 @@ public class ElevatorSubsystem extends SubsystemBase {
    */
   public void setGoal(GoalState goalState) {
     this.goalState = goalState;
+    pidController.setGoal(new TrapezoidProfile.State(goalState.getPosition(), 0));
+    timer.reset();  
   }
 
   @Override
   public void periodic() {
     currentPosition = encoder.getPosition();
+    currentVelocity = encoder.getVelocity();
+    currentAngle = angle.get();
+
+    if (goalState == GoalState.ACQUIRE && currentPosition <= GoalState.ACQUIRE.getPosition()){
+      motor.stopMotor();
+      return;
+    }
+
+    TrapezoidProfile.State state = profile.calculate(timer.get());
+    double output = pidController.calculate(currentVelocity, state.velocity);
+    double feedforward = this.feedforward.calculate(currentVelocity, state.velocity) + (Math.cos(currentAngle.getRadians()) * KG);
+    motor.setVoltage(output + feedforward);
   }
 }
--- src/main/java/frc/robot/subsystems/Subsystems.java
+++ src/main/java/frc/robot/subsystems/Subsystems.java
@@ -4,10 +4,14 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+
 /** Container class that manages robot subsystems. */
 public class Subsystems {
   public final SwerveSubsystem drivetrain = new SwerveSubsystem();
   public final ClawSubsystem claw = new ClawSubsystem();
   public final PhotonVisionSubsystem photonVision = new PhotonVisionSubsystem();
   public final ColorSensorSubsystem colorSensor = new ColorSensorSubsystem();
+  public final ElevatorAngleSubsystem elevatorAngle = new ElevatorAngleSubsystem();
+  public final ElevatorSubsystem elevator = new ElevatorSubsystem(() -> Rotation2d.fromDegrees(elevatorAngle.getAngle()));
 }
