1f5dfadd3c7257ac39a54403dd8b4c03d1cb0736
Ed Reed <reed.ej@hotmail.com>
2023-02-12 18:43:55 -0800

    Fix scoreToGrid drive vector calculations

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,11 +10,15 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -40,8 +44,8 @@ public final class Constants {
     /**
      * CAN Ids.
      */
-    public static class CAN{
-      public static class SparkMax{
+    public static class CAN {
+      public static class SparkMax {
         public static final int INTAKE = 1;
         public static final int ELEVATOR = 2;
         public static final int ELEVATOR_ANGLE = 3;
@@ -54,12 +58,31 @@ public final class Constants {
      * TODO: Determine location of camera on the competition robot.
      */
     public static final Transform3d CAMERA_TO_ROBOT = new Transform3d(
-            new Translation3d(Units.inchesToMeters(-28.0), Units.inchesToMeters(2.0), Units.inchesToMeters(-25.6)),
-            new Rotation3d());
+        new Translation3d(Units.inchesToMeters(-28.0), Units.inchesToMeters(2.0), Units.inchesToMeters(-25.6)),
+        new Rotation3d());
+
     /**
      * A transform from the robot center to the camera.
      */
     public static final Transform3d ROBOT_TO_CAMERA = CAMERA_TO_ROBOT.inverse();
+
+    /**
+     * A constant representing the distance from the grid to position the robot for
+     * scoring a game element.
+     */
+    public static final double SCORING_DISTANCE_FROM_GRID = Units.inchesToMeters(15);
+
+    /**
+     * The robot length including bumpers.
+     * 
+     * TODO: Determine competition robot length.
+     */
+    public static final double ROBOT_LENGTH = Units.inchesToMeters(38);
+
+    /**
+     * The offset from the center of the grid to side scoring position.
+     */
+    public static final double GRID_SIDE_OFFSET = Units.inchesToMeters(22);
   }
 
   /**
--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -20,7 +20,6 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -89,6 +88,16 @@ public final class Autos {
    * Returns a command sequence that drives the robot to the grid based on the
    * detected AprilTag and selected alignment (left, center or right), and scores
    * the acquired game element.
+   * <p>
+   * The manipulator selects a grid scoring position from the Xbox controller's
+   * d-pad. When the d-pad is not pressed, it selects the center middle of the
+   * grid. Pressing the d-pad diagonally up, side and diagonally down select the
+   * left and right high, middle and lower scoring grids, respectively. The
+   * selection is indicated on the Operator tab of Shuffleboard. When a selection
+   * is made, the manipulator presses and holds the right bumper to execute the
+   * scoring sequence.
+   * <p>
+   * TODO: Finalize the scoring sequence selection interface with the manipulator.
    * 
    * @param subsystems            The subsystems container.
    * @param manipulatorController The manipulator's Xbox controller.
@@ -96,12 +105,14 @@ public final class Autos {
    * @return A command sequence to drive the robot to the grid and score.
    */
   public static Command scoreToGrid(Subsystems subsystems, XboxController manipulatorController) {
+    var drivetrain = subsystems.drivetrain;
+
     var target = subsystems.photonVision.getBestTarget();
     var cameraToTarget = target.getBestCameraToTarget();
-    var robotPose = subsystems.drivetrain.getPosition3d();
 
     // Transform the robot's pose to find the tag's pose
-    var cameraPose = robotPose.transformBy(RobotConstants.CAMERA_TO_ROBOT);
+    var robotPose = drivetrain.getPosition3d();
+    var cameraPose = robotPose.transformBy(RobotConstants.ROBOT_TO_CAMERA);
     System.out.println("CAMERA POSE = " + cameraPose);
     var targetPose = cameraPose.transformBy(cameraToTarget);
     System.out.println("TARGET POSE = " + targetPose);
@@ -111,25 +122,25 @@ public final class Autos {
 
     int pov = manipulatorController.getPOV();
     if (pov >= 45 && pov <= 145) {
-      yOffset = Units.inchesToMeters(22);
+      // Left side
+      yOffset = -RobotConstants.GRID_SIDE_OFFSET;
     } else if (pov >= 225 && pov <= 315) {
-      yOffset = Units.inchesToMeters(-22);
+      // Right side
+      yOffset = RobotConstants.GRID_SIDE_OFFSET;
     }
 
     // Find the scoring position pose.
     var tagToGoal = new Transform3d(
-        new Translation3d(Units.inchesToMeters(15), yOffset, 0.0),
+        new Translation3d(RobotConstants.SCORING_DISTANCE_FROM_GRID + (RobotConstants.ROBOT_LENGTH / 2), yOffset, 0.0),
         new Rotation3d(0, 0, Math.PI));
     var goalPose = targetPose.transformBy(tagToGoal).toPose2d();
 
     System.out.println("GOAL POSE = " + goalPose);
 
-    Translation2d translationToGoal = goalPose.getTranslation();
-    Translation2d driveVector = new Translation2d(translationToGoal.getNorm(), translationToGoal.getAngle());
+    var driveVector = goalPose.relativeTo(robotPose.toPose2d()).getTranslation();
 
     return Commands.sequence(
-        new ProfiledDriveStraight(subsystems.drivetrain, driveVector, subsystems.drivetrain.getMaxSpeed() * 0.5,
-            new Rotation2d(0))
+        new ProfiledDriveStraight(drivetrain, driveVector, drivetrain.getMaxSpeed() * 0.5, new Rotation2d(0))
     // TODO: Add scoring sequence here
     );
   }
