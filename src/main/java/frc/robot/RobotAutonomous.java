2053164c7bbfdfcd032e87980072f3583c0056dc
Ed Reed <reed.ej@hotmail.com>
2023-02-18 14:06:38 -0800

    Convert to 2 space indentation

--- src/main/java/frc/robot/RobotAutonomous.java
+++ src/main/java/frc/robot/RobotAutonomous.java
@@ -20,103 +20,103 @@ import frc.robot.subsystems.Subsystems;
  */
 public class RobotAutonomous {
 
-    /**
-     * An enum representing the delay, in seconds, before the autonomous routine
-     * starts. This can be used to avoid collisions with alliance robots when we
-     * know their autonomous path would cross ours.
-     */
-    private static enum ChooseAutoDelay {
-        NO_DELAY(0),
-        DELAY_2_SECONDS(2),
-        DELAY_5_SECONDS(5);
+  /**
+   * An enum representing the delay, in seconds, before the autonomous routine
+   * starts. This can be used to avoid collisions with alliance robots when we
+   * know their autonomous path would cross ours.
+   */
+  private static enum ChooseAutoDelay {
+    NO_DELAY(0),
+    DELAY_2_SECONDS(2),
+    DELAY_5_SECONDS(5);
 
-        private int delay;
-
-        /**
-         * Creates a new ChooseAuthoDelay.
-         * 
-         * @param delay The delay in seconds.
-         */
-        ChooseAutoDelay(int delay) {
-            this.delay = delay;
-        }
-
-        /**
-         * Returns the delay in seconds.
-         * 
-         * @return The delay in seconds.
-         */
-        public int getDelay() {
-            return delay;
-        }
-
-        /**
-         * Returns the name of this constant.
-         */
-        @Override
-        public String toString() {
-            return String.format("%d seconds", delay);
-        }
-    }
-
-    private final Subsystems subsystems;
-    private final SendableChooser<Command> autonomousCommandChooser;
-    private final SendableChooser<ChooseAutoDelay> autoDelayChooser = new SendableChooser<>();
+    private int delay;
 
     /**
-     * Creates a new RobotAutonomous.
+     * Creates a new ChooseAuthoDelay.
      * 
-     * @param subsystems The subsystems container.
+     * @param delay The delay in seconds.
      */
-    public RobotAutonomous(Subsystems subsystems) {
-        this.subsystems = subsystems;
-
-        autonomousCommandChooser = Autonomous.getChooser(this.subsystems, "frc.robot");
-        Autos.getPathfinderCommands(subsystems)
-                .stream()
-                .forEach(path -> autonomousCommandChooser.addOption(path.getLabel(), path.getValue()));
-
-        for (ChooseAutoDelay delay : EnumSet.allOf(ChooseAutoDelay.class)) {
-            autoDelayChooser.addOption(delay.toString(), delay);
-        }
-
-        autoDelayChooser.setDefaultOption(ChooseAutoDelay.NO_DELAY.toString(), ChooseAutoDelay.NO_DELAY);
+    ChooseAutoDelay(int delay) {
+      this.delay = delay;
     }
 
     /**
-     * Returns the autonomous command selected in the chooser.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        // Use ProxyCommand to run the autonomous routine. This allow multiple runs of
-        // the same command with potentially different initial delays. It is mainly
-        // useful for debugging.
-        return getSelectedDelayCommand().andThen(new ProxyCommand(autonomousCommandChooser.getSelected()));
-    }
-
-    /**
-     * Returns a {@link WaitCommand} used to wait before starting the autonomous
-     * routine.
+     * Returns the delay in seconds.
      * 
-     * @return A {@link WaitCommand} that waits for the selected delay.
+     * @return The delay in seconds.
      */
-    private Command getSelectedDelayCommand() {
-        ChooseAutoDelay delayChoice = autoDelayChooser.getSelected();
-        return new WaitCommand(delayChoice.getDelay());
+    public int getDelay() {
+      return delay;
     }
 
     /**
-     * Adds the autonomous layout to the Shuffleboard tab.
-     * 
-     * @param layout The layout to add user interface elements.
+     * Returns the name of this constant.
      */
-    public ShuffleboardLayout addShuffleboardLayout(ShuffleboardTab tab) {
-        ShuffleboardLayout autonomousLayout = tab.getLayout("Autonomous", BuiltInLayouts.kList);
-
-        autonomousLayout.add("Routine", autonomousCommandChooser);
-        autonomousLayout.add("Delay", autoDelayChooser);
-
-        return autonomousLayout;
+    @Override
+    public String toString() {
+      return String.format("%d seconds", delay);
     }
+  }
+
+  private final Subsystems subsystems;
+  private final SendableChooser<Command> autonomousCommandChooser;
+  private final SendableChooser<ChooseAutoDelay> autoDelayChooser = new SendableChooser<>();
+
+  /**
+   * Creates a new RobotAutonomous.
+   * 
+   * @param subsystems The subsystems container.
+   */
+  public RobotAutonomous(Subsystems subsystems) {
+    this.subsystems = subsystems;
+
+    autonomousCommandChooser = Autonomous.getChooser(this.subsystems, "frc.robot");
+    Autos.getPathfinderCommands(subsystems)
+        .stream()
+        .forEach(path -> autonomousCommandChooser.addOption(path.getLabel(), path.getValue()));
+
+    for (ChooseAutoDelay delay : EnumSet.allOf(ChooseAutoDelay.class)) {
+      autoDelayChooser.addOption(delay.toString(), delay);
+    }
+
+    autoDelayChooser.setDefaultOption(ChooseAutoDelay.NO_DELAY.toString(), ChooseAutoDelay.NO_DELAY);
+  }
+
+  /**
+   * Returns the autonomous command selected in the chooser.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // Use ProxyCommand to run the autonomous routine. This allow multiple runs of
+    // the same command with potentially different initial delays. It is mainly
+    // useful for debugging.
+    return getSelectedDelayCommand().andThen(new ProxyCommand(autonomousCommandChooser.getSelected()));
+  }
+
+  /**
+   * Returns a {@link WaitCommand} used to wait before starting the autonomous
+   * routine.
+   * 
+   * @return A {@link WaitCommand} that waits for the selected delay.
+   */
+  private Command getSelectedDelayCommand() {
+    ChooseAutoDelay delayChoice = autoDelayChooser.getSelected();
+    return new WaitCommand(delayChoice.getDelay());
+  }
+
+  /**
+   * Adds the autonomous layout to the Shuffleboard tab.
+   * 
+   * @param layout The layout to add user interface elements.
+   */
+  public ShuffleboardLayout addShuffleboardLayout(ShuffleboardTab tab) {
+    ShuffleboardLayout autonomousLayout = tab.getLayout("Autonomous", BuiltInLayouts.kList);
+
+    autonomousLayout.add("Routine", autonomousCommandChooser);
+    autonomousLayout.add("Delay", autoDelayChooser);
+
+    return autonomousLayout;
+  }
 }
