84e4722a546ccb0431251eadf872df7ddee0acaf
Ed Reed <reed.ej@hotmail.com>
2023-02-27 19:46:27 -0800

    Fix JavaDoc

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -208,10 +208,10 @@ public final class Autos {
    * name to a {@link Command} to follow a path group stored in the PathPlanner
    * deployment directory.
    * <p>
-   * Multiple pathplanner files with the naming convention &quot;&lt;origin&gt;[-Score
-   * &lt;n&gt;]&quot; will be combined into a single sequence. The path with name
-   * "&lt;origin&gt;" will be followed unconditionally. The path with name
-   * "&lt;origin&gt;-Score &lt;n&gt;" will be selected based on the number of game
+   * Multiple pathplanner files with the naming convention &quot;&lt;_origin&gt;[-Score
+   * &lt;_n&gt;]&quot; will be combined into a single sequence. The path with name
+   * &quot;&lt;_origin&gt;&quot; will be followed unconditionally. The path with name
+   * &quot;&lt;_origin&gt;-Score &lt;_n&gt;&quot; will be selected based on the number of game
    * elements to score.
    * <p>
    * Files the do not conform to the naming convention described above will be
