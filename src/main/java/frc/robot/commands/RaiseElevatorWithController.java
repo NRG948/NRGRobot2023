e25ab6344eb28b6ff0789b93a1d027eb2bc00d1e
NRG Programmer <newportrobotics@gmail.com>
2023-03-07 20:58:50 -0800

    Moved elevator limit switch into ElevatorSubsystem.

--- src/main/java/frc/robot/commands/RaiseElevatorWithController.java
+++ src/main/java/frc/robot/commands/RaiseElevatorWithController.java
@@ -51,13 +51,7 @@ public class RaiseElevatorWithController extends CommandBase {
   public void execute() {
     double speed = -controller.getLeftY();
     speed = MathUtil.applyDeadband(speed, 0.05) * MAX_POWER;
-    // Stops the motors if the elevator is at the top or bottom limit.
-    if (speed < 0 && elevator.atBottomLimit()
-        || speed > 0 && elevator.atTopLimit()) {
-      elevator.stopMotor();
-    } else {
-      elevator.setMotorVoltage(speed * RobotConstants.MAX_BATTERY_VOLTAGE);
-    }
+    elevator.setMotorVoltage(speed * RobotConstants.MAX_BATTERY_VOLTAGE);
   }
 
   // Called once the command ends or is interrupted.
