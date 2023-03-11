6d7a311b8836d0b878d1d5dfbecbb1fb2dabfcec
Ed Reed <reed.ej@hotmail.com>
2023-03-11 09:12:57 -0800

    Set indentation to 2 spaces

--- src/main/java/frc/robot/MatchLogger.java
+++ src/main/java/frc/robot/MatchLogger.java
@@ -9,51 +9,51 @@ import edu.wpi.first.wpilibj.DriverStation;
 /** Add your docs here. */
 public class MatchLogger {
 
-    /**
-     * Prints Tele-Op starts now
-     */
-    public void printTeleOpStart() {
-        System.out.println("\n Tele-Op starts now!\n");
-    }
+  /**
+   * Prints Tele-Op starts now
+   */
+  public void printTeleOpStart() {
+    System.out.println("\n Tele-Op starts now!\n");
+  }
 
-    /**
-     * Prints Autonomous starts now
-     */
-    public void printAutoStart() {
-        System.out.println("\n Autonomous starts now!\n");
-    }
+  /**
+   * Prints Autonomous starts now
+   */
+  public void printAutoStart() {
+    System.out.println("\n Autonomous starts now!\n");
+  }
 
-    /**
-     * Prints if the robot has been emergency-stopped.
-     */
-    public void printIfEStopped() {
-        if (DriverStation.isEStopped()) {
-            System.out.println("Robot has been E-Stopped!!! \n Robot has been E-Stopped \n");
-        }
+  /**
+   * Prints if the robot has been emergency-stopped.
+   */
+  public void printIfEStopped() {
+    if (DriverStation.isEStopped()) {
+      System.out.println("Robot has been E-Stopped!!! \n Robot has been E-Stopped \n");
     }
+  }
 
-    /**
-     * Print match details -
-     * ex. Qualification match number: 8
-     * Red alliance drivers' station: 3
-     */
-    public void printMatchDetails() {
-        System.out.println(
-            "\nmatch type: " + DriverStation.getMatchType() + 
-            "\nmatch number: " + DriverStation.getMatchNumber() + 
-            "\nalliance: " + DriverStation.getAlliance() + 
+  /**
+   * Print match details -
+   * ex. Qualification match number: 8
+   * Red alliance drivers' station: 3
+   */
+  public void printMatchDetails() {
+    System.out.println(
+        "\nmatch type: " + DriverStation.getMatchType() +
+            "\nmatch number: " + DriverStation.getMatchNumber() +
+            "\nalliance: " + DriverStation.getAlliance() +
             "\ndrivers' station: " + DriverStation.getLocation());
-    }
+  }
 
-    public void printMatchTimeLeft() {
-        System.out.println("Match Time Remaining: " + DriverStation.getMatchTime());
-    }
+  public void printMatchTimeLeft() {
+    System.out.println("Match Time Remaining: " + DriverStation.getMatchTime());
+  }
 
-    public void printReplayNumber() { // TODO: Understand what the replay number is...and when we would call it.
-        System.out.println(DriverStation.getReplayNumber());
-    }
+  public void printReplayNumber() { // TODO: Understand what the replay number is...and when we would call it.
+    System.out.println(DriverStation.getReplayNumber());
+  }
 
-    public void printGameSpecificMessage() {
-        DriverStation.getGameSpecificMessage();
-    }
+  public void printGameSpecificMessage() {
+    DriverStation.getGameSpecificMessage();
+  }
 }
