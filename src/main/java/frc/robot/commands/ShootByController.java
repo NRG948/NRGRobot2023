5ca22d2e83cfe2f45430efa223ed8b9590269827
NRG Programmer <newportrobotics@gmail.com>
2023-03-21 22:08:24 -0700

    fixed typo

--- src/main/java/frc/robot/commands/ShootByController.java
+++ src/main/java/frc/robot/commands/ShootByController.java
@@ -40,7 +40,7 @@ public class ShootByController extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    ShooterSubsystem.stopMotor();
+    shooterSubsystem.stopMotor();
 
   }
 
