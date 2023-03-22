a7746c8da17de54d695e80919c6826acfff9c75b
Nilesh <45780409+thaknil24@users.noreply.github.com>
2023-03-21 20:06:32 -0700

    Added more methods and Javadoc. Co-Authored-By: Harry695 Co-Authored-By: GuestUser1000101 Co-Authored-By: griffeyy07

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,24 +7,25 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.RobotConstants.CAN;
 
 public class ShooterSubsystem extends SubsystemBase {
 
-  private static final double MAX_RPM = 500.0; // change name if reed doesnt like it
+  private static final double MAX_RPM = 5880.0; // change name if reed doesnt like it
   private static final double RPM_PER_VOLT = 493.9; // Provided by systems, the change in RPM per change in volt. Could be useful.
-  private static final double INDEXER_RPM = 200.0; // TODO: get real RPMs
+  private static final double KS = 1.0; // guess
 
   private static final double BACKSPIN_FACTOR = 0.9; // TODO: determine real backspin factor
 
   public enum GoalShooterRPM {
     // TODO: get real RPMs
     STOP(0.0),
-    HYBRID_RPM_PERCENT(0.2),
-    MID_RPM_PERCENT(0.45),
-    HIGH_RPM_PERCENT(0.75);
+    HYBRID_RPM(250),
+    MID_RPM(1000), // 1000 rpm as proposed by Taiga
+    HIGH_RPM(1500);
 
     private final double rpm;
 
@@ -33,37 +34,30 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     /**
-     * Returns the current RPM of the shooter.
+     * Returns the RPM corresponding to the desired goal.
      * 
-     * @return The current RPM of the scooter.
+     * @return The RPM corresponding to the desired goal.
      */
-    private double getGoalShooterRPM() {
+    private double getRPM() {
       return rpm;
     }
   }
 
-
-  private static CANSparkMax indexerMotor = new CANSparkMax(CAN.SparkMax.INDEXER, MotorType.kBrushless);
   private static CANSparkMax topShooterMotor = new CANSparkMax(CAN.SparkMax.TOP_SHOOTER, MotorType.kBrushless);
   private static CANSparkMax bottomShooterMotor = new CANSparkMax(CAN.SparkMax.BOTTOM_SHOOTER, MotorType.kBrushless);
+  private static RelativeEncoder encoder = topShooterMotor.getEncoder();
 
   private GoalShooterRPM currentGoalRPM = GoalShooterRPM.STOP;
+  
+  private double currentRPM;
   private boolean isEnabled = false;
 
   /** Creates a new ShooterSubsystem. */
   public ShooterSubsystem() {
-    indexerMotor.setIdleMode(IdleMode.kBrake);
-    topShooterMotor.setIdleMode(IdleMode.kBrake);
-    bottomShooterMotor.setIdleMode(IdleMode.kBrake);
+    topShooterMotor.setIdleMode(IdleMode.kCoast);
+    bottomShooterMotor.setIdleMode(IdleMode.kCoast);
   }
-
-  public void periodic() {
-    if (isEnabled) {
-      setIndexerMotorPower(INDEXER_RPM);
-      setShooterMotorPower(currentGoalRPM.getGoalShooterRPM());
-    }
-  }
-
+  
   /**
    * Sets the goal RPM of the shooter.
    * 
@@ -72,39 +66,86 @@ public class ShooterSubsystem extends SubsystemBase {
   public void setGoalRPM(GoalShooterRPM goalRPM) {
     currentGoalRPM = goalRPM;
   }
-
+  
   /**
    * Sets the motor speed for the shooter motors.
    * 
    * @param speed The desired motor speed.
    */
-  public void setShooterMotorPower(double speed) {
-    bottomShooterMotor.set(speed);
-    topShooterMotor.set(speed * BACKSPIN_FACTOR); // spin bottom motor slower to create backspin
+  public void setMotorVoltage(double voltage) {
+    bottomShooterMotor.setVoltage(voltage);
+    topShooterMotor.setVoltage(voltage * BACKSPIN_FACTOR); // spin bottom motor slower to create backspin
   }
-
+  
   /**
-   * Sets the motor speed for the indexer motor.
+   * Enables the shooter.
    * 
-   * @param speed The desired motor speed.
+   * @param goalShooterRPM The desired shooter RPM.
    */
-  public void setIndexerMotorPower(double speed) {
-    indexerMotor.set(speed);
-  }
-
-  public void shoot(GoalShooterRPM GoalRPM) {
+  public void enable(GoalShooterRPM goalShooterRPM) {
     isEnabled = true;
-    currentGoalRPM = GoalRPM;
+    setGoalRPM(goalShooterRPM);
   }
-
+  
+  /**
+   * Calculates the motor voltage needed for a given RPM.
+   * 
+   * @param goalShooterRPM The desired RPM.
+   * @return The motor voltage needed.
+   */
+  public double calculateMotorVoltage(GoalShooterRPM goalShooterRPM) {
+    return goalShooterRPM.getRPM() / RPM_PER_VOLT + KS; // not sure
+  }
+  
+  /**
+   * Stops the motors.
+   */
   public static void stopMotor() {
-    indexerMotor.stopMotor();
     topShooterMotor.stopMotor();
     bottomShooterMotor.stopMotor();
   }
-
+  
+  /**
+   * Disable the shooter.
+   */
   public void disable() {
     isEnabled = false;
     stopMotor();
   }
+
+  /**
+   * Returns the current RPM.
+   * 
+   * @return the current RPM.
+   */
+  public double getCurrentRPM() {
+    return currentRPM;
+  }
+  
+  /**
+   * Returns the current goal RPM.
+   * 
+   * @return the current goal RPM.
+   */
+  public GoalShooterRPM getCurrentGoalRPM() {
+    return currentGoalRPM;
+  }
+
+  /**
+   * Returns whether the shooter is enabled.
+   * 
+   * @return whether the shooter is enabled.
+   */
+  public boolean isEnabled() {
+    return isEnabled;
+  }
+
+  public void periodic() {
+    currentRPM = encoder.getVelocity();
+
+    if (isEnabled) {
+      setMotorVoltage(calculateMotorVoltage(currentGoalRPM));
+    }
+  }
+
 }
