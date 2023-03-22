fb7de2ad0b4131fb0eff6c386abe83ee996f9a10
Nilesh <45780409+thaknil24@users.noreply.github.com>
2023-03-22 07:38:28 -0700

    Updated scoring to remove elevator

--- src/main/java/frc/robot/commands/Scoring.java
+++ src/main/java/frc/robot/commands/Scoring.java
@@ -12,12 +12,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants.RobotConstants;
-import frc.robot.subsystems.ClawSubsystem;
-import frc.robot.subsystems.ClawSubsystem.Position;
-// import frc.robot.subsystems.ElevatorAngleSubsystem;
-// import frc.robot.subsystems.ElevatorAngleSubsystem.ElevatorAngle;
-// import frc.robot.subsystems.ElevatorSubsystem;
-// import frc.robot.subsystems.ElevatorSubsystem.GoalState;
 import frc.robot.subsystems.Subsystems;
 import frc.robot.subsystems.SwerveSubsystem;
 
