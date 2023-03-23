af9d7168a5fff814dc91a575f49deff1b8b15d25
Ed Reed <reed.ej@hotmail.com>
2023-03-23 15:40:41 -0700

    Remove unused color sensor subsystem

--- src/main/java/frc/robot/subsystems/Subsystems.java
+++ src/main/java/frc/robot/subsystems/Subsystems.java
@@ -4,14 +4,12 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 /** Container class that manages robot subsystems. */
 public class Subsystems {
   public final SwerveSubsystem drivetrain = new SwerveSubsystem();
   public final PhotonVisionSubsystem photonVision = new PhotonVisionSubsystem();
-  public final ColorSensorSubsystem colorSensor = new ColorSensorSubsystem();
   public final IntakeSubsystem intake = new IntakeSubsystem();
   public final AdressableLEDSubsystem leds = new AdressableLEDSubsystem();
   public final ShooterSubsystem shooter = new ShooterSubsystem();
@@ -20,7 +18,6 @@ public class Subsystems {
   public final Subsystem[] all = new Subsystem[] {
     drivetrain,
     photonVision,
-    colorSensor,
     shooter,
     indexer,
     intake,
